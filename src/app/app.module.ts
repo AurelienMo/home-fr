12afb73963f740b52256542b4fac4f4f8b47b198
Aurelien Morvan <morvan.aurelien@gmail.com>
2019-03-01 23:32:11 +0100

    [Components] Add Spinner component & add this on AppComponent to display spinner on each page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,26 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { HeaderComponent } from './components/header/header.component';
+import { ContactComponent } from './components/contact/contact.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NgxSpinnerModule} from 'ngx-spinner';
+import { SpinnerComponent } from './modules/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactComponent,
+    SpinnerComponent,
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     MDBBootstrapModulePro.forRoot(),
     IconsModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    NgxSpinnerModule
   ],
   providers: [
     MDBSpinningPreloader,
